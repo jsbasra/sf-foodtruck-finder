79a346822a073758ef4b5d303eac52479ad12665
jsbasra <46688535+jsbasra@users.noreply.github.com>
2021-06-30 22:38:50 -0400

    Update foodtruck.spec.ts

--- tests/foodtruck.spec.ts
+++ tests/foodtruck.spec.ts
@@ -1,15 +1,11 @@
 const { webkit, devices } = require('@playwright/test');
 test('basic test', async ({ page }) => {
-
-  const iPhone11 = devices['iPhone 11 Pro'];
-  (async () => {
-  const browser = await webkit.launch();
+  
   const context = await browser.newContext({
-    ...iPhone11,
-    locale: 'en-US',
-    geolocation: { longitude: 12.492507, latitude: 41.889938 },
-    permissions: ['geolocation']
+  geolocation: { longitude: 48.858455, latitude: 2.294474 },
+  permissions: ['geolocation']
   });
+  
   const page = await context.newPage();
   await page.goto('https://basra.win');
   expect(await page.title()).toContain('SF FoodTrucks');
@@ -17,6 +13,6 @@ test('basic test', async ({ page }) => {
   await page.screenshot({ path: 'map.png' });
   await browser.close();
   expect(page.url()).toBe('https://basra.win/');
+  //await context.setGeolocation({ longitude: 29.979097, latitude: 31.134256 });
 
 });
-
