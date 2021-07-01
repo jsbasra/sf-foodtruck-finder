ede188c5b749828c0331bd05005bdee7c9d65e3e
jsbasra <46688535+jsbasra@users.noreply.github.com>
2021-06-30 22:30:28 -0400

    Update and rename foo.spec.ts to foodtruck.spec.ts

--- tests/foodtruck.spec.ts
+++ tests/foodtruck.spec.ts
@@ -1,5 +1,6 @@
 const { webkit, devices } = require('@playwright/test');
 test('basic test', async ({ page }) => {
+
   const iPhone11 = devices['iPhone 11 Pro'];
   (async () => {
   const browser = await webkit.launch();
@@ -11,8 +12,11 @@ test('basic test', async ({ page }) => {
   });
   const page = await context.newPage();
   await page.goto('https://basra.win');
+  expect(await page.title()).toContain('SF FoodTrucks');
   await page.click('text="Display Food Trucks"');
   await page.screenshot({ path: 'map.png' });
   await browser.close();
   expect(page.url()).toBe('https://basra.win/');
-})();
+
+});
+
