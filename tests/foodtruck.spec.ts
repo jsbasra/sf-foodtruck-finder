0445a3c196ffcc9a09c6fda312a58b7f701cc189
jsbasra <46688535+jsbasra@users.noreply.github.com>
2021-07-01 09:37:24 -0400

    Update foodtruck.spec.ts

--- tests/foodtruck.spec.ts
+++ tests/foodtruck.spec.ts
@@ -2,7 +2,7 @@ const { test, expect, chromium } = require('@playwright/test');
 test('basic test', async () => {
   const browser = await chromium.launch();
   const context = await browser.newContext({
-  geolocation: { longitude: 48.858455, latitude: 2.294474 },
+  geolocation: { longitude: -122.452672, latitude: 37.754043 },
   permissions: ['geolocation']
   });
   
@@ -11,6 +11,14 @@ test('basic test', async () => {
   expect(await page.title()).toContain('SF FoodTrucks');
   await page.click('text="Display Food Trucks"');
   await page.screenshot({ path: 'map.png' });
+   const [msg] = await Promise.all([
+    page.waitForEvent('console'),
+    page.evaluate(() => {
+      // Issue console.log statements inside the page.
+      console.log(37.74563668388077);
+    }),
+  ]);
+  await msg.args[0].jsonValue() 
   await browser.close();
   expect(page.url()).toBe('https://basra.win/');
   //await context.setGeolocation({ longitude: 29.979097, latitude: 31.134256 });
